224e34ec28e1577f22b369dbe094420da632b281
akashTharuka <akash_tharuka@yahoo.com>
2024-09-24 19:33:26 +0530

    first commit

---
+++ src/main/java/com/dfs/dfsmasterserver/DfsMasterServerApplication.java
@@ -0,0 +1,13 @@
+package com.dfs.dfsmasterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsMasterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfsMasterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dfs/dfsmasterserver/DfsMasterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfs.dfsmasterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsMasterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
