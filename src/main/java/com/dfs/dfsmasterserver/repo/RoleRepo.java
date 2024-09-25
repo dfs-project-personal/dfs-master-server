c2f76e494f5e4b1605463ee31cb9f8cb0e1d2ee5
akashTharuka <akash_tharuka@yahoo.com>
2024-09-25 17:09:37 +0530

    setup env vars

--- src/main/java/com/dfs/dfsmasterserver/repo/RoleRepo.java
+++ src/main/java/com/dfs/dfsmasterserver/repo/RoleRepo.java
@@ -2,7 +2,9 @@ package com.dfs.dfsmasterserver.repo;
 
 import com.dfs.dfsmasterserver.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepo extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
--- src/main/java/com/dfs/dfsmasterserver/repo/UserRepo.java
+++ src/main/java/com/dfs/dfsmasterserver/repo/UserRepo.java
@@ -2,7 +2,9 @@ package com.dfs.dfsmasterserver.repo;
 
 import com.dfs.dfsmasterserver.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
 }
